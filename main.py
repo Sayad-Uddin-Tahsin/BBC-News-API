0742ae5c8278e62e7ccd87df19d8a8aebee76f30
Sayad Uddin Tahsin <89304780+Sayad-Uddin-Tahsin@users.noreply.github.com>
2023-03-18 14:04:05 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ import json
 import logging
 import pytz
 from datetime import datetime
-from dotenv import dotenv_values
-
-secrets = dotenv_values(".env")
 
 
 # ================ LOGGING INITIATION ================
@@ -375,18 +372,9 @@ async def log():
                           mimetype="text/html; charset=utf-8",
                           status=200)
 
-
-@app.route('/start/')
-@app.route('/start/<pin>')
-async def start(pin):
-    if int(pin) == int(secrets['PIN']):
-        __startBackgroundCaching()
-        t2 = threading.Thread(target=__checkExpired)
-        t2.daemon = True
-        t2.start()
-        return {'operation': 'Successful'}
-    else:
-        return {'operation': 'Failed'}
-
 if __name__ == "__main__":
+    __startBackgroundCaching()
+    t2 = threading.Thread(target=__checkExpired)
+    t2.daemon = True
+    t2.start()
     app.run(host="0.0.0.0", port=8080, debug=False)
