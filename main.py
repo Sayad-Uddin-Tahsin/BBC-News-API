d0bd6f60fa430bdfe794e409b4cef35a3ed5ace5
Sayad Uddin Tahsin <89304780+Sayad-Uddin-Tahsin@users.noreply.github.com>
2024-02-28 13:05:48 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import threading
 import flask
 from flask import Flask
 from requests_html import HTMLSession
@@ -9,7 +8,7 @@ import pytz
 from datetime import datetime
 import os
 
-open('log.txt', 'w').close()
+open("log.txt", "w").close()
 
 
 # ================ LOGGING INITIATION ================
@@ -18,8 +17,9 @@ logger.setLevel(logging.INFO)
 
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
-console_format = logging.Formatter("[%(levelname)s] %(message)s",
-                                   datefmt="%Y-%m-%d %H:%M:%S")
+console_format = logging.Formatter(
+    "[%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
 console_handler.setFormatter(console_format)
 
 file_handler = logging.FileHandler("log.txt")
@@ -29,9 +29,9 @@ file_handler.setFormatter(file_format)
 
 
 class NoFlaskFilter(logging.Filter):
-  def filter(self, record):
-    message = record.getMessage()
-    return not ('HTTP/1.1' in message and ('GET' in message or 'OPTIONS *'))
+    def filter(self, record):
+        message = record.getMessage()
+        return not ("HTTP/1.1" in message and ("GET" in message or "OPTIONS *"))
 
 
 console_handler.addFilter(NoFlaskFilter())
@@ -45,9 +45,10 @@ session = HTMLSession()
 
 # ================ DHAKA TIME ================
 def ctime():
-  timezone = pytz.timezone('Asia/Dhaka')
-  ctime = datetime.now(timezone).strftime('%Y-%m-%d %H:%M:%S ')
-  return ctime
+    timezone = pytz.timezone("Asia/Dhaka")
+    ctime = datetime.now(timezone).strftime("%Y-%m-%d %H:%M:%S ")
+    return ctime
+
 
 # ---------------- URL Dict ----------------
 urls = {
@@ -79,7 +80,7 @@ urls = {
     "pashto": "https://bbc.com/pashto",
     "sinhala": "https://bbc.com/sinhala",
     "tamil": "https://bbc.com/tamil",
-    "uzbek": "https://bbc.com/uzbek"
+    "uzbek": "https://bbc.com/uzbek",
 }
 
 # ---------------- DOC HTML ----------------
@@ -147,22 +148,23 @@ def main():
     return flask.Response(
         json.dumps({"status": "OK"}, ensure_ascii=False),
         mimetype="application/json; charset=utf-8",
-        status=200
+        status=200,
     )
 
-@app.route('/doc')
-@app.route('/doc/')
+
+@app.route("/doc")
+@app.route("/doc/")
 async def doc():
     logger.info(f"{ctime()}: [ENDPOINT] DOC endpoint called - 200")
     return Info
 
-def _get1(lang):
+
+def _get1(lang, latest):
     start = int(time.time())
     r = session.get(lang)
     match = r.html.find("div.bbc-1ajedpd")
     response = {}
-    response['status'] = 200
-    latest = True
+    response["status"] = 200
     try:
         sections = match[0].find("section")
         sections.pop(len(sections) - 1)
@@ -172,39 +174,41 @@ def _get1(lang):
                 news = []
                 lis = section.find("li")
                 for li in lis:
-                    promo_image = li.find("div.promo-image")[0].find("img")[0].attrs['src']
+                    promo_image = (
+                        li.find("div.promo-image")[0].find("img")[0].attrs["src"]
+                    )
                     promo_text = li.find("div.promo-text")[0].find("h3")[0]
                     title = promo_text.text
                     link = list(promo_text.absolute_links)[0]
-                    news.append({
-                    "title": str(title),
-                    "news_link": str(link),
-                    "image_link": str(promo_image)
-                    })
+                    news.append(
+                        {
+                            "title": str(title),
+                            "news_link": str(link),
+                            "image_link": str(promo_image),
+                        }
+                    )
                 if news:
                     response[section_name[0].text.strip()] = news
                     if latest:
-                        response['latest'] = {}
-                        response['latest'][section_name[0].text.strip()] = news
-                        latest = False
+                        break
         end = int(time.time())
         duration = end - start
-        response['elapsed time'] = f"{duration:.2f}s"
-        response['last update'] = int(time.time())
-        response['timestamp'] = int(time.time())
+        response["elapsed time"] = f"{duration:.2f}s"
+        response["last update"] = int(time.time())
+        response["timestamp"] = int(time.time())
     except IndexError:
         pass
     return response
 
-def _get2(lang):
+
+def _get2(lang, latest):
     start = int(time.time())
     r = session.get(lang)
     div = r.html.find("div", first=True)
     response = {}
-    response['status'] = 200
+    response["status"] = 200
     sections = div.find("section")
     sections.pop(len(sections) - 1)
-    latest = True
     for section in sections:
         section_name = section.find("h2")
         if section_name:
@@ -212,15 +216,22 @@ def _get2(lang):
             lis = section.find("li.ebmt73l0")
             for li in lis:
                 try:
-                    promo_image = li.find("picture", first=True).find("source")[0].attrs['srcset'].split(", ")[-1]
+                    promo_image = (
+                        li.find("picture", first=True)
+                        .find("source")[0]
+                        .attrs["srcset"]
+                        .split(", ")[-1]
+                    )
                     promo_text = li.find("h3")[0]
                     title = promo_text.text
                     link = list(promo_text.absolute_links)[0]
-                    news.append({
-                    "title": str(title),
-                    "news_link": str(link),
-                    "image_link": str(promo_image)
-                    })
+                    news.append(
+                        {
+                            "title": str(title),
+                            "news_link": str(link),
+                            "image_link": str(promo_image),
+                        }
+                    )
                 except IndexError:
                     continue
                 except AttributeError:
@@ -228,100 +239,96 @@ def _get2(lang):
             if news:
                 response[section_name[0].text.strip()] = news
                 if latest:
-                    response['latest'] = {}
-                    response['latest'][section_name[0].text.strip()] = news
-                    latest = False
+                    break
     end = int(time.time())
     duration = end - start
-    response['elapsed time'] = f"{duration:.2f}s"
-    response['last update'] = int(time.time())
-    response['timestamp'] = int(time.time())
+    response["elapsed time"] = f"{duration:.2f}s"
+    response["last update"] = int(time.time())
+    response["timestamp"] = int(time.time())
     return response
 
-@app.route('/', defaults={'language': None})
-@app.route('/<language>/', defaults={'type': None})
-@app.route('/<language>/<type>/')
+
+@app.route("/", defaults={"language": None})
+@app.route("/<language>/", defaults={"type": None})
+@app.route("/<language>/<type>/")
 async def news(language, type):
     if str(language).lower() not in urls:
-        return flask.Response(json.dumps(
-        {
-            "status": 400,
-            "error": "Invalid Language!",
-            "languages":
-            f"https://{(flask.request.url).split('/')[2]}/doc#languages"
-        },
-        ensure_ascii=False).encode('utf8'),
-                            mimetype="application/json; charset=utf-8",
-                            status=400)
-    
-    if str(type) == 'news':
-        response = _get1(urls[str(language).lower()])
+        return flask.Response(
+            json.dumps(
+                {
+                    "status": 400,
+                    "error": "Invalid Language!",
+                    "languages": f"https://{(flask.request.url).split('/')[2]}/doc#languages",
+                },
+                ensure_ascii=False,
+            ).encode("utf8"),
+            mimetype="application/json; charset=utf-8",
+            status=400,
+        )
+
+    if str(type) == "news":
+        response = _get1(urls[str(language).lower()], False)
         if len(response.keys()) < 4:
-            response = _get2(urls[str(language).lower()])
+            response = _get2(urls[str(language).lower()], False)
         response.pop("latest")
         logger.info(
             f"{ctime()}: [ENDPOINT] NEWS (language: {language}, type: {type}) endpoint called - 200"
         )
-        return flask.Response(json.dumps(response, ensure_ascii=False).encode('utf8'),
-                          mimetype="application/json; charset=utf-8",
-                          status=200)
-    elif str(type) == 'latest':
-        response = _get1(urls[str(language).lower()])
+        return flask.Response(
+            json.dumps(response, ensure_ascii=False).encode("utf8"),
+            mimetype="application/json; charset=utf-8",
+            status=200,
+        )
+    elif str(type) == "latest":
+        response = _get1(urls[str(language).lower()], True)
         if len(response.keys()) < 4:
-            response = _get2(urls[str(language).lower()])
-        
-        latest_response = {}
-        for key in response:
-            if key != "latest":
-                latest_response[key] = response[key]
-            else:
-                latest_response[list(response["latest"].keys())[0]] = response[list(response["latest"].keys())[0]]
+            response = _get2(urls[str(language).lower()], True)
+
         logger.info(
             f"{ctime()}: [ENDPOINT] NEWS (language: {language}, type: {type}) endpoint called - 200"
         )
-        return flask.Response(json.dumps(latest_response, ensure_ascii=False).encode('utf8'),
-                            mimetype="application/json; charset=utf-8",
-                            status=200)
-    else:
-        logger.info(
-        f"{ctime()}: [ENDPOINT] NEWS (language: {language}) endpoint called - 400 (Invalid Type)"
+        return flask.Response(
+            json.dumps(response, ensure_ascii=False).encode("utf8"),
+            mimetype="application/json; charset=utf-8",
+            status=200,
         )
-        return flask.Response(json.dumps(
-        {
-            'status': 400,
-            'error': 'Invalid Type!',
-            'types': ['news', 'latest']
-        },
-        ensure_ascii=False).encode('utf8'),
-                            mimetype="application/json; charset=utf-8",
-                            status=400)
-    
-@app.route('/log/', defaults={'pin': None})
-@app.route('/log/<pin>')
-@app.route('/logs/', defaults={'pin': None})
-@app.route('/logs/<pin>')
-async def log(pin):
-    if pin != None and int(pin) == int(os.environ['PIN']):
-        with open("log.txt", 'r', encoding="utf-8") as f:
-            logs = f.read()
-        logs = logs.replace('\n', '<br>')
-        logger.info(f"{ctime()}: [ENDPOINT] LOG endpoint called - 200")
-        return flask.Response(logs,
-                            mimetype="text/html; charset=utf-8",
-                            status=200)
     else:
         logger.info(
-        f"{ctime()}: [ENDPOINT] LOG endpoint called - 400 (Authorization Failed)"
+            f"{ctime()}: [ENDPOINT] NEWS (language: {language}) endpoint called - 400 (Invalid Type)"
         )
         return flask.Response(
-        json.dumps({
-            'status': 400,
-            'error': 'Authorization Failed'
-        },
-                    ensure_ascii=False),
-        mimetype="application/json; charset=utf-8",
-        status=400,
+            json.dumps(
+                {"status": 400, "error": "Invalid Type!", "types": ["news", "latest"]},
+                ensure_ascii=False,
+            ).encode("utf8"),
+            mimetype="application/json; charset=utf-8",
+            status=400,
         )
 
+
+@app.route("/log/", defaults={"pin": None})
+@app.route("/log/<pin>")
+@app.route("/logs/", defaults={"pin": None})
+@app.route("/logs/<pin>")
+async def log(pin):
+    if pin != None and int(pin) == int(os.environ["PIN"]):
+        with open("log.txt", "r", encoding="utf-8") as f:
+            logs = f.read()
+        logs = logs.replace("\n", "<br>")
+        logger.info(f"{ctime()}: [ENDPOINT] LOG endpoint called - 200")
+        return flask.Response(logs, mimetype="text/html; charset=utf-8", status=200)
+    else:
+        logger.info(
+            f"{ctime()}: [ENDPOINT] LOG endpoint called - 400 (Authorization Failed)"
+        )
+        return flask.Response(
+            json.dumps(
+                {"status": 400, "error": "Authorization Failed"}, ensure_ascii=False
+            ),
+            mimetype="application/json; charset=utf-8",
+            status=400,
+        )
+
+
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8080, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=False)
